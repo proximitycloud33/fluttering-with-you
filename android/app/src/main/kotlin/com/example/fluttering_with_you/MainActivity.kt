1b786d561d304f2409cba2d01a3c5c5a745e29bb
Fathur Nor Alif <fathurnoralif@gmail.com>
2021-10-14 11:25:42 +0800

    First Commit

---
+++ android/app/src/main/kotlin/com/example/fluttering_with_you/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttering_with_you
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fluttering_with_you/screens/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fluttering With You',
+      home: HomePage(),
+    );
+  }
+}
